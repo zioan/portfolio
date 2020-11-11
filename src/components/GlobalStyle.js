06a2ca4b399a81e8cfacc25ddaa63d46da4550d6
Zaharia Ioan <zaharia.ioan@gmail.com>
2020-11-11 13:25:49 +0100

    styled components AboutUs page components

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from "styled-components";
+//this needs to be imported in App.js
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
+  }
+
+  button{
+    font-family: 'Inter', sans-serif;
+    font-weight: bold;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    outline: none;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+  }
+
+  button:hover{
+    background: #23d997;
+  }
+
+  h2{
+    font-weight: lighter;
+    font-size: 4rem;
+  }
+  h3{
+    color: white;
+  }
+  h4{
+    font-weight: bold;
+    font-size: 2rem;
+  }
+  a{
+    font-size: 1.1rem;
+  }
+  span{
+    font-weight: bold;
+    color:#23d997;
+  }
+  p{
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+  }
+
+`;
+
+export default GlobalStyle;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,15 +4,18 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+//styled components
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services.
         </h2>
-        <div className="cards">
+        <Cards>
           <div className="card">
             <div className="icon">
               <img src={clock} alt="clock" />
@@ -41,13 +44,41 @@ const ServicesSection = () => {
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
-      </div>
-      <div className="image">
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="home2" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+//Instead of styling a new div you can style an existing styled div. This will import all the styling applied to the About styled component.
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  .card {
+    flex-basis: 20rem;
+  }
+  .icon {
+    display: flex;
+    align-items: center;
+  }
+  h3 {
+    margin-left: 1rem;
+    background: white;
+    color: black;
+    padding: 1rem;
+  }
+`;
 
 export default ServicesSection;
