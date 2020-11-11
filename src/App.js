dffdaf4893ad71bd3291ea374bed04b7ffb09b4f
Zaharia Ioan <zaharia.ioan@gmail.com>
2020-11-11 17:12:38 +0100

    router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
 import Nav from "./components/Nav";
+
+//Router
+import { Switch, Route, Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
