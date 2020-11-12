7f27d3160108fa993ed19c477c9440581c27e25e
Zaharia Ioan <zaharia.ioan@gmail.com>
2020-11-12 16:38:00 +0100

    animation 1

--- src/App.js
+++ src/App.js
@@ -8,30 +8,35 @@ import ContactUs from "./pages/ContactUs";
 import MovieDetail from "./pages/MovieDetail";
 
 import Nav from "./components/Nav";
+//AnimatePresence - it (animate) detects when browsing away. Also is using the useLocation from react-router-dom
+import { AnimatePresence } from "framer-motion";
 
 //Router
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        {/* this :id works like a dynamic text */}
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          {/* this :id works like a dynamic text */}
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
